783b7c1e4b5dff14c2843a271a5c969af5ea14d0
ee2221 <galgut.prit@gmail.com>
2025-06-01 13:56:20 -0400

    Remove Help Popup

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,6 @@ function App() {
       <Toolbar />
       <LayersPanel />
       <ObjectProperties />
-      <div className="absolute bottom-4 left-4 bg-white/90 rounded-lg shadow-lg p-4">
-        <h2 className="text-lg font-semibold mb-2">3D Modeling App</h2>
-        <p className="text-sm text-gray-600">
-          Left click + drag to rotate view<br />
-          Right click + drag to pan<br />
-          Scroll to zoom
-        </p>
-      </div>
     </div>
   );
 }
